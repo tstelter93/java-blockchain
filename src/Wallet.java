3949bdba5839edab1988fed0e541f58d891b8ac3
Tim Stelter <tstelter@vt.edu>
2018-05-22 12:04:54 -0400

    Added ability to generate public/private keys for our wallet.

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,4 +1,5 @@
 import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 /**
  * This class functions like a wallet for cryptocurrency.
@@ -10,6 +11,39 @@ public class Wallet {
 
 	private PrivateKey privateKey;
 	private PublicKey publicKey;
+	
+	/**
+	 * Constructor for the Wallet Class.
+	 */
+	public Wallet() {
+		generateKeyPair();
+	}
+	
+	/**
+	 * Generates a public and private key pair based on Elliptic-curve cryptograhpy.
+	 * https://en.wikipedia.org/wiki/Elliptic-curve_cryptography
+	 */
+	public void generateKeyPair() {
+		try {
+			// Setup
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");  // What do these parameters mean?
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			
+			// Elliptic-curve KeyPir spec
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+			
+			// Initialize the key generator and generate a KeyPair
+			keyGen.initialize(ecSpec, random);
+			KeyPair keyPair = keyGen.generateKeyPair();
+			
+			// Set the public and private keys from the keyPair
+			privateKey = keyPair.getPrivate();
+			publicKey = keyPair.getPublic();
+			
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 	
 	/**
 	 * Get the private key.
